186032be38dc71910823420d95f36aa261ceefbc
kn <nguyentuanpkn@gmail.com>
2023-09-13 16:27:36 +0700

    Add FileService

---
+++ Services/FileService.cs
@@ -0,0 +1,102 @@
+namespace Ndknitor.Services;
+public interface IFileService
+{
+    Stream Get(string url);
+    Task<string> Insert(IFormFile file, string url = null);
+    Task<string> Update(IFormFile file, string url = null);
+    Task<string> Upsert(IFormFile file, string url = null);
+    Task<bool> Delete(string url);
+}
+public class ApplicationFileService : IFileService
+{
+    private readonly IWebHostEnvironment _environment;
+    public ApplicationFileService(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+    public async Task<bool> Delete(string fileName)
+    {
+        string savePath = Path.Combine(_environment.WebRootPath, fileName);
+        if (File.Exists(savePath))
+        {
+            File.Delete(savePath);
+            return true;
+        }
+        return false;
+    }
+
+    public Stream Get(string url)
+    {
+        string savePath = Path.Combine(_environment.WebRootPath, url);
+        if (File.Exists(savePath))
+        {
+            return File.OpenRead(savePath);
+        }
+        return null;
+    }
+
+    public async Task<string> Insert(IFormFile file, string url = null)
+    {
+        url = url ?? file.FileName;
+        string savePath = Path.Combine(_environment.WebRootPath, url);
+        string directoryPath = Path.GetDirectoryName(savePath);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        if (!File.Exists(savePath))
+        {
+            using (Stream sourcestream = file.OpenReadStream())
+            {
+                using (FileStream desStream = File.Create(savePath))
+                {
+                    await sourcestream.CopyToAsync(desStream);
+                }
+            }
+            return url;
+        }
+        return null;
+    }
+
+    public async Task<string> Update(IFormFile file, string url)
+    {
+        url = url ?? file.FileName;
+        string savePath = Path.Combine(_environment.WebRootPath, url);
+        string directoryPath = Path.GetDirectoryName(savePath);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        if (File.Exists(savePath))
+        {
+            using (Stream sourcestream = file.OpenReadStream())
+            {
+                using (FileStream desStream = File.Create(savePath))
+                {
+                    await sourcestream.CopyToAsync(desStream);
+                }
+            }
+            return url;
+        }
+        return null;
+    }
+
+    public async Task<string> Upsert(IFormFile file, string url)
+    {
+        url = url ?? file.FileName;
+        string savePath = Path.Combine(_environment.WebRootPath, url);
+        string directoryPath = Path.GetDirectoryName(savePath);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        using (Stream sourcestream = file.OpenReadStream())
+        {
+            using (FileStream desStream = File.Create(savePath))
+            {
+                await sourcestream.CopyToAsync(desStream);
+            }
+        }
+        return url;
+    }
+}
