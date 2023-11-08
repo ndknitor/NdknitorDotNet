bd104d53eb9c0617f5b7bd54301147fd6ffb7f97
kn <nguyentuanpkn@gmail.com>
2023-11-08 09:55:06 +0700

    Bson, rename ToClass to ToJsonClass

--- System/ObjectExtension.cs
+++ System/ObjectExtension.cs
@@ -1,9 +1,20 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
 namespace Ndknitor.System;
 public static class ObjectExtensions
 {
     public static string ToJson(this object obj)
     {
         return JsonConvert.SerializeObject(obj);
+    }
+    public static string ToBson(this object value)
+    {
+        using (MemoryStream ms = new MemoryStream())
+        using (BsonDataWriter datawriter = new BsonDataWriter(ms))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(datawriter, value);
+            return Convert.ToBase64String(ms.ToArray());
+        }
     }
 }
--- System/StringExtension.cs
+++ System/StringExtension.cs
@@ -1,9 +1,21 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
 namespace Ndknitor.System;
 public static class StringExtension
 {
-    public static T ToClass<T>(this string str)
+    public static T ToJsonClass<T>(this string str)
     {
         return JsonConvert.DeserializeObject<T>(str);
+    }
+    public static T ToBsonClass<T>(string base64data)
+    {
+        byte[] data = Convert.FromBase64String(base64data);
+
+        using (MemoryStream ms = new MemoryStream(data))
+        using (BsonDataReader reader = new BsonDataReader(ms))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            return serializer.Deserialize<T>(reader);
+        }
     }
 }
