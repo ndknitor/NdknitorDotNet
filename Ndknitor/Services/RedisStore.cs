cd27c33915a7babad1c6a6d8350ff4cadecf7c0d
kn <nguyentuanpkn@gmail.com>
2023-11-16 07:52:04 +0700

    Redis store

--- Ndknitor/Services/RedisStore.cs
+++ Ndknitor/Services/RedisStore.cs
@@ -1,14 +1,6 @@
-using System.Security.Claims;
-using StackExchange.Redis;
-using Ndknitor.System;
-using System.Text;
-using Microsoft.Extensions.Options;
-
-namespace Ndknitor.Services.Web;
-
-public static class RedisSessionServiceCollectionExtensions
+public static class RedisStoreServiceCollectionExtensions
 {
-    public static void AddRedisSession(this IServiceCollection services, Action<RedisSessionOptions> configureOptions = null)
+    public static void AddRedisStore(this IServiceCollection services, Action<RedisSessionOptions> configureOptions = null)
     {
         services.AddSingleton<IRedisSession, RedisSession>();
         services.AddSingleton<GlobalRedisSession>();
@@ -23,7 +15,8 @@ public class RedisSessionOptions
 {
     public TimeSpan? Timeout { get; set; }
 }
-public interface IRedisSession
+
+public interface IRedisStore
 {
     public TimeSpan Timeout { get; }
     public T Get<T>(string key);
@@ -38,17 +31,12 @@ public interface IRedisSession
     public async Task<IEnumerable<string>> GetAllKeysAsync();
 }
 
-public class RedisSession : IRedisSession
+public class RedisStore
 {
-    private readonly HttpContext context;
     private readonly IDatabase redis;
-    private readonly string prefix;
-
-    public RedisSession(IHttpContextAccessor accessor, IDatabase redis, IOptions<RedisSessionOptions> options)
+    public RedisStore(IHttpContextAccessor accessor, IDatabase redis, IOptions<RedisSessionOptions> options)
     {
         this.redis = redis;
-        context = accessor.HttpContext;
-        prefix = $"{nameof(RedisSession)}{context.User.FindFirstValue(ClaimTypes.NameIdentifier)}";
         Timeout = options.Value.Timeout ?? TimeSpan.FromMinutes(30); // Set a default timeout if not provided
     }
 
@@ -56,7 +44,6 @@ public class RedisSession : IRedisSession
 
     public T Get<T>(string key)
     {
-        key = prefix + key;
         var value = redis.StringGet(key);
         if (value.HasValue)
         {
@@ -67,7 +54,6 @@ public class RedisSession : IRedisSession
 
     public async Task<T> GetAsync<T>(string key)
     {
-        key = prefix + key;
         var value = await redis.StringGetAsync(key);
         if (value.HasValue)
         {
@@ -78,47 +64,30 @@ public class RedisSession : IRedisSession
 
     public bool Set(string key, object value, TimeSpan? expiry = null)
     {
-        key = prefix + key;
         return redis.StringSet(key, value.ToBson(), expiry ?? Timeout);
     }
 
     public async Task<bool> SetAsync(string key, object value, TimeSpan? expiry = null)
     {
-        key = prefix + key;
         return await redis.StringSetAsync(key, value.ToBson(), expiry ?? Timeout);
     }
 
     public bool Exist(string key)
     {
-        key = prefix + key;
         return redis.KeyExists(key);
     }
 
     public async Task<bool> ExistAsync(string key)
     {
-        key = prefix + key;
         return await redis.KeyExistsAsync(key);
     }
     public bool Remove(string key)
     {
-        key = prefix + key;
         return redis.KeyDelete(key);
     }
 
     public async Task<bool> RemoveAsync(string key)
     {
-        key = prefix + key;
         return await redis.KeyDeleteAsync(key);
-    }
-    public IEnumerable<string> GetAllKeys()
-    {
-        var keys = redis.Execute("KEYS", $"{prefix}*"); // Prefix with your identifier
-        return keys.ToDictionary().Keys;
-    }
-
-    public async Task<IEnumerable<string>> GetAllKeysAsync()
-    {
-        var keys = await redis.ExecuteAsync("KEYS", $"{prefix}*"); // Prefix with your identifier
-        return keys.ToDictionary().Keys;
     }
 }
