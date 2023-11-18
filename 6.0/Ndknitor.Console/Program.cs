610393c006ffc144a1a70b365a5f4f02e4a43d0b
kn <nguyentuanpkn@gmail.com>
2023-11-18 08:32:41 +0700

    Console

--- 6.0/Ndknitor.Console/Program.cs
+++ 6.0/Ndknitor.Console/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics;
 using Ndknitor.System;
 using Newtonsoft.Json;
-
+Console.WriteLine("");
 // int[] arrayToSort = { 4, 2, 7, 1, 9, 5, 3, 8, 6 };
 
 // arrayToSort = arrayToSort.ParallelMergeSort();
