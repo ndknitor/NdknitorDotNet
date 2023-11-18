62719958a1db91e904eeed496d3908159d181904
kn <nguyentuanpkn@gmail.com>
2023-11-18 07:57:33 +0700

    Rename

--- 6.0/Ndknitor.Console/Program.cs
+++ 6.0/Ndknitor.Console/Program.cs
@@ -1,43 +1,12 @@
-﻿Console.WriteLine("Dit me may");
-// using System.ComponentModel.DataAnnotations;
-// using Ndknitor.System;
-// using Ndknitor.Web.Validations;
-// using StackExchange.Redis;
-// var regularTextAttribute = new RegularTextAttribute
+﻿using System.Diagnostics;
+using Ndknitor.System;
+using Newtonsoft.Json;
+
+// int[] arrayToSort = { 4, 2, 7, 1, 9, 5, 3, 8, 6 };
+
+// arrayToSort = arrayToSort.ParallelMergeSort();
+
+// foreach (var item in arrayToSort)
 // {
-//     IncludeSpace = true,
-//     IncludeNumber = false,
-//     IncludeCharaters = "!@#"
-// };
-
-// // Act
-// bool isValid = regularTextAttribute.IsValid("Valid_Text");
-// Console.WriteLine(isValid);
-
-
-
-// public class TestModel
-// {
-//     public int Value { get; set; }
-//     public int ComparisonValue { get; set; }
+//     Console.WriteLine(item);
 // }
-
-//Console.WriteLine(result.ErrorMessage);
-
-
-// var obj = new
-// {
-//     Name = "Movie Premiere",
-//     StartDate = new DateTime(2013, 1, 22, 20, 30, 0, DateTimeKind.Utc),
-//     Description = "Hello"
-// };
-// using (var multiplexer = ConnectionMultiplexer.Connect("localhost:6379"))
-// {
-//     var db = multiplexer.GetDatabase();
-//     db.StringSet("1", obj.ToJson(), TimeSpan.FromSeconds(69));
-//     db.StringSet("2", obj.ToBson(), TimeSpan.FromSeconds(69));
-// }
-// string json = obj.ToJson();
-
-// Console.WriteLine($"{json} : {json.Length}");
-// Console.WriteLine(obj.ToBson().Length);
