86d5b670cbf50906b8d540887ff3de63909f937b
ndkn <nguyentuanpkn@gmail.com>
2023-11-19 14:26:05 +0700

    optimize STARS-914

--- 6.0/Ndknitor.Console/Program.cs
+++ 6.0/Ndknitor.Console/Program.cs
@@ -1,12 +1,39 @@
-﻿using System.Diagnostics;
-using Ndknitor.System;
-using Newtonsoft.Json;
-Console.WriteLine("");
-// int[] arrayToSort = { 4, 2, 7, 1, 9, 5, 3, 8, 6 };
+﻿using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.AspNetCore.Authentication;
+using Ndknitor.Services.Web;
 
-// arrayToSort = arrayToSort.ParallelMergeSort();
+BenchmarkRunner.Run<KeyBasedCookieDataFormatBenchmark>();
 
-// foreach (var item in arrayToSort)
-// {
-//     Console.WriteLine(item);
-// }
+
+public class KeyBasedCookieDataFormatBenchmark
+{
+    private readonly KeyBasedCookieDataFormat cookieDataFormat;
+    private readonly AuthenticationTicket authenticationTicket;
+
+    public KeyBasedCookieDataFormatBenchmark()
+    {
+        string authenticationKey = "YourAuthenticationKey";
+        cookieDataFormat = new KeyBasedCookieDataFormat(authenticationKey);
+
+        var claimsIdentity = new ClaimsIdentity(new[] { new Claim("claim1", "value1"), new Claim("claim2", "value2") });
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        authenticationTicket = new AuthenticationTicket(claimsPrincipal, "TestScheme");
+    }
+
+    [Benchmark]
+    public string Protect()
+    {
+        return cookieDataFormat.Protect(authenticationTicket, "purpose");
+    }
+
+    [Benchmark]
+    public AuthenticationTicket Unprotect()
+    {
+        string protectedText = cookieDataFormat.Protect(authenticationTicket, "purpose");
+        return cookieDataFormat.Unprotect(protectedText, "purpose");
+    }
+}
--- 6.0/Ndknitor/Services/KeyBasedCookieDataFormat.cs
+++ 6.0/Ndknitor/Services/KeyBasedCookieDataFormat.cs
@@ -5,12 +5,21 @@ namespace Ndknitor.Services.Web;
 public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
 {
     private readonly byte[] encryptionKey;
+    private readonly Aes aesAlg;
+    private ICryptoTransform encryptor;
+    private ICryptoTransform decryptor;
 
     public KeyBasedCookieDataFormat(string authenticationKey)
     {
         using (var sha256 = SHA256.Create())
         {
             encryptionKey = sha256.ComputeHash(Encoding.UTF8.GetBytes(authenticationKey));
+            aesAlg = Aes.Create();
+            aesAlg.Key = encryptionKey;
+            aesAlg.Mode = CipherMode.ECB;
+            aesAlg.Padding = PaddingMode.PKCS7;
+            encryptor = aesAlg.CreateEncryptor();
+            decryptor = aesAlg.CreateDecryptor();
         }
     }
 
@@ -26,11 +35,6 @@ public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
             throw new ArgumentNullException(nameof(data));
         }
 
-        using var aesAlg = Aes.Create();
-        aesAlg.Key = encryptionKey;
-        aesAlg.Mode = CipherMode.ECB;
-        aesAlg.Padding = PaddingMode.PKCS7;
-        using var encryptor = aesAlg.CreateEncryptor();
         var ticketSerializer = new TicketSerializer();
         var ticketBytes = ticketSerializer.Serialize(data);
         string protectedText = Convert.ToBase64String(encryptor.TransformFinalBlock(ticketBytes, 0, ticketBytes.Length));
@@ -51,13 +55,6 @@ public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
         }
         try
         {
-            using var aesAlg = Aes.Create();
-            aesAlg.Key = encryptionKey;
-            aesAlg.Mode = CipherMode.ECB;
-            aesAlg.Padding = PaddingMode.PKCS7;
-
-            using var decryptor = aesAlg.CreateDecryptor();
-
             var encryptedBytes = Convert.FromBase64String(protectedText);
             var decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
 
