dd0498468b9c5f9172e57a81129e3b0b755d272b
kn <nguyentuanpkn@gmail.com>
2023-12-07 10:41:09 +0700

    PBKDF2Hasher

---
+++ 8.0/Ndknitor.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Text;
+using Ndknitor.System;
+
+PBKDF2Hasher hasher = new PBKDF2Hasher{
+
+};
+byte[] password = Encoding.UTF8.GetBytes("123456");
+byte[] hash = hasher.Hash(password);
+Console.WriteLine(BitConverter.ToString(hash).ToLower().Replace("-", null));
+Console.WriteLine(hasher.Verify(password, hash));
+
---
+++ 8.0/Ndknitor/System/PBKDF2Hasher.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Security.Cryptography;
+namespace Ndknitor.System;
+public class PBKDF2Hasher
+{
+    public int Iteration { get; set; } = 1000000;
+    public int SaltSize { get; set; } = 32;
+    public int HashSize { get; set; } = 32;
+    public HashAlgorithmName AlgorithmName { get; set; } = HashAlgorithmName.SHA512;
+    public string Hash(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+        using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iteration, AlgorithmName))
+        {
+            var hash = pbkdf2.GetBytes(HashSize);
+            var combinedBytes = new byte[salt.Length + hash.Length];
+            Array.Copy(salt, 0, combinedBytes, 0, salt.Length);
+            Array.Copy(hash, 0, combinedBytes, salt.Length, hash.Length);
+            return Convert.ToBase64String(combinedBytes);
+        }
+    }
+    public byte[] Hash(byte[] password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+        using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iteration, AlgorithmName))
+        {
+            var hash = pbkdf2.GetBytes(HashSize);
+            var combinedBytes = new byte[salt.Length + hash.Length];
+            Array.Copy(salt, 0, combinedBytes, 0, salt.Length);
+            Array.Copy(hash, 0, combinedBytes, salt.Length, hash.Length);
+            return combinedBytes;
+        }
+    }
+    public bool Verify(byte[] password, byte[] hashedPassword)
+    {
+        var salt = new byte[SaltSize];
+        var hash = new byte[HashSize];
+        Array.Copy(hashedPassword, 0, salt, 0, salt.Length);
+        Array.Copy(hashedPassword, salt.Length, hash, 0, hash.Length);
+        using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iteration, AlgorithmName))
+        {
+            var enteredHash = pbkdf2.GetBytes(HashSize);
+            return StructuralComparisons.StructuralEqualityComparer.Equals(hash, enteredHash);
+        }
+    }
+    public bool Verify(string password, string hashedPassword)
+    {
+        var combinedBytes = Convert.FromBase64String(hashedPassword);
+        var salt = new byte[SaltSize];
+        var hash = new byte[HashSize];
+        Array.Copy(combinedBytes, 0, salt, 0, salt.Length);
+        Array.Copy(combinedBytes, salt.Length, hash, 0, hash.Length);
+        using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iteration, AlgorithmName))
+        {
+            var enteredHash = pbkdf2.GetBytes(HashSize);
+            return StructuralComparisons.StructuralEqualityComparer.Equals(hash, enteredHash);
+        }
+    }
+}
