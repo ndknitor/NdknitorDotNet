eb85fc40ad6b03641813017912c5ec40a9ae8633
kn <nguyentuanpkn@gmail.com>
2024-01-04 14:44:43 +0700

    IV for KeyBasedCookieDataFormat

--- 8.0/Ndknitor/Services/KeyBasedCookieDataFormat.cs
+++ 8.0/Ndknitor/Services/KeyBasedCookieDataFormat.cs
@@ -2,27 +2,18 @@ using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Authentication;
 namespace Ndknitor.Services.Web;
+using Base58Check;
 /// <summary>
 /// This provide cookie authentication with the ability to use a customizable encryption key.
 /// </summary>
 public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
 {
-    private readonly byte[] encryptionKey;
-    private readonly Aes aesAlg;
-    private ICryptoTransform encryptor;
-    private ICryptoTransform decryptor;
-
+    private readonly byte[] key;
     public KeyBasedCookieDataFormat(string authenticationKey)
     {
         using (var sha256 = SHA256.Create())
         {
-            encryptionKey = sha256.ComputeHash(Encoding.UTF8.GetBytes(authenticationKey));
-            aesAlg = Aes.Create();
-            aesAlg.Key = encryptionKey;
-            aesAlg.Mode = CipherMode.ECB;
-            aesAlg.Padding = PaddingMode.PKCS7;
-            encryptor = aesAlg.CreateEncryptor();
-            decryptor = aesAlg.CreateDecryptor();
+            key = sha256.ComputeHash(Encoding.UTF8.GetBytes(authenticationKey));
         }
     }
 
@@ -33,14 +24,9 @@ public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
 
     public string Protect(AuthenticationTicket data, string purpose)
     {
-        if (data == null)
-        {
-            throw new ArgumentNullException(nameof(data));
-        }
-
         var ticketSerializer = new TicketSerializer();
         var ticketBytes = ticketSerializer.Serialize(data);
-        string protectedText = Convert.ToBase64String(encryptor.TransformFinalBlock(ticketBytes, 0, ticketBytes.Length));
+        string protectedText = Base58CheckEncoding.Encode(EncryptAes(ticketBytes));
         return protectedText;
 
     }
@@ -52,14 +38,10 @@ public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
 
     public AuthenticationTicket Unprotect(string protectedText, string purpose)
     {
-        if (string.IsNullOrEmpty(protectedText))
-        {
-            throw new ArgumentNullException(nameof(protectedText));
-        }
         try
         {
-            var encryptedBytes = Convert.FromBase64String(protectedText);
-            var decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+            var encryptedBytes = Base58CheckEncoding.Decode(protectedText);
+            var decryptedBytes = DecryptAes(encryptedBytes);
 
             var ticketSerializer = new TicketSerializer();
             var ticket = ticketSerializer.Deserialize(decryptedBytes);
@@ -71,4 +53,45 @@ public class KeyBasedCookieDataFormat : ISecureDataFormat<AuthenticationTicket>
         }
 
     }
+    byte[] EncryptAes(byte[] plain)
+    {
+        byte[] result = null;
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Padding = PaddingMode.ISO10126;
+            aesAlg.GenerateIV();
+            aesAlg.Key = key;
+            using (ICryptoTransform encryptor = aesAlg.CreateEncryptor())
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    ms.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        cs.Write(plain, 0, plain.Length);
+                        cs.FlushFinalBlock();
+                    }
+                    result = ms.ToArray();
+                }
+            }
+        }
+        return result;
+    }
+
+    byte[] DecryptAes(byte[] clipher)
+    {
+        byte[] result = null;
+        using (Aes aesAlg = Aes.Create())
+        {
+            byte[] iv = new byte[16];
+            Buffer.BlockCopy(clipher, 0, iv, 0, iv.Length);
+            aesAlg.Padding = PaddingMode.ISO10126;
+            aesAlg.Key = key;
+            aesAlg.IV = iv;
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor();
+            result = decryptor.TransformFinalBlock(clipher, iv.Length, clipher.Length - iv.Length);
+        }
+        return result;
+    }
+
 }
